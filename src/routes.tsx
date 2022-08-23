0f7e05cd7ba58cbba13f423a04779a8f51cfe0d9
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-08-23 15:55:57 -0300

    add: criando página de denúncia (issue #9)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import App from "./App";
-
+import Report from "./pages/Report";
 
 const Router = () => {
-    return(
+    return (
         <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/job" element={<App/>} />
+            <Route path="/" element={<Login />} />
+            <Route path="/job" element={<App />} />
+            <Route path="/report" element={<Report />} />
         </Routes>
     );
 };
 
 export default Router;
+
